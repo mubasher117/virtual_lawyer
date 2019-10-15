33b0be17861215eb8e7b9da1656e671c7910c3d3
Mubasher Ahmad <mubasherahmad012345@gmail.com>
2019-10-15 22:48:06 +0500

    Added home and about pages.

--- views.py
+++ views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'virtual_lawyer/index.html')
+
+def about(request):
+    return render(request,'virtual_lawyer/about.html')
