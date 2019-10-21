6ec0b0fcc1f51a97105165121a46194e535706f7
Hunia Fatima <huniafatima99@gmail.com>
2019-10-21 05:09:31 -0700

    ChatBot Integration with upadting DOM failure

--- models.py
+++ models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from chatterbot import ChatBot
+from chatterbot.ext.django_chatterbot import settings
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from django.apps import apps
 
-# Create your models here.
+class Chatbot(models.Model):
+    bot = None
+    @classmethod
+    def get_ChatBot(self):
+        if self.bot is None:
+            self.bot = ChatBot('Norman')
+            trainer = ChatterBotCorpusTrainer(self.bot)
+            trainer.train("chatterbot.corpus.english")
+        return self.bot
+    # @classmethod
+    # def load(cls):
+    #     try:
+    #         return cls.objects.get()
+    #     except cls.DoesNotExist:
+    #         return cls()
--- views.py
+++ views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.base import TemplateView
+from django.views.generic import View
+from django.http import JsonResponse
+from .models import Chatbot
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
@@ -7,5 +12,16 @@ def index(request):
 def about(request):
     return render(request,'virtual_lawyer/about.html')
 
+@csrf_exempt
 def chatbot(request):
-    return(render(request, 'virtual_lawyer/chatbot.html'))
+    myBot = Chatbot.get_ChatBot()
+    botResponse = ''
+    if request.method == "POST":
+        User = request.POST["talk"]
+        botResponse = myBot.get_response(User)
+        print(botResponse)
+        print("printing bot response***************************************")
+    return render(request, 'virtual_lawyer/chatbot.html', {"BotResponse": botResponse})
+
+    # return JsonResponse(data)
+    
